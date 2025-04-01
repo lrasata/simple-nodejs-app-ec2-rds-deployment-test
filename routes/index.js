17d96f94a41927d56c96372ad9c19a826f71348f
Liantsoa Rasata <rmliantsoa@gmail.com>
2025-04-01 11:58:59 +0200

    Revert "update sequelize auth"
    This reverts commit 55405b559b472a10e5bc2308b030233728485f2e.

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,7 @@ const sequelize = require('../models/index.js');
 
 router.get('/status', async (req, res) => {
     try {
-        await sequelize
-            .authenticate()
-            .then(() => {
-                console.log('Connection has been established successfully.\n')
-            })
-            .catch(err => {
-                console.error('Unable to connect to the database:', err)
-            })
+        await sequelize.authenticate();
         res.json({ message: 'Connection has been established successfully.' });
     } catch (error) {
         res.status(500).json({ message: 'Unable to connect to the database.', error });
