55405b559b472a10e5bc2308b030233728485f2e
Liantsoa Rasata <rmliantsoa@gmail.com>
2025-04-01 11:49:32 +0200

    update sequelize auth

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,14 @@ const sequelize = require('../models/index.js');
 
 router.get('/status', async (req, res) => {
     try {
-        await sequelize.authenticate();
+        await sequelize
+            .authenticate()
+            .then(() => {
+                console.log('Connection has been established successfully.\n')
+            })
+            .catch(err => {
+                console.error('Unable to connect to the database:', err)
+            })
         res.json({ message: 'Connection has been established successfully.' });
     } catch (error) {
         res.status(500).json({ message: 'Unable to connect to the database.', error });
